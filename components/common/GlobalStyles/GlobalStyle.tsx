42603c7d171ea5f7541e89c382dce3b706930f0a
Vasha Faris <vashafaris@gmail.com>
2021-07-10 15:02:31 +0700

    feat: setup global style

---
+++ components/common/GlobalStyles/GlobalStyle.tsx
@@ -0,0 +1,79 @@
+import { createGlobalStyle } from 'styled-components';
+
+import { DEVICE_SIZE } from '@constants/device-size.constant';
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
+
+  html {
+    height: 100%;
+    font-size: 62.5%;
+    box-sizing: border-box;
+
+    @media screen and (max-width: ${DEVICE_SIZE.laptop}) {
+      font-size: 56.25%;
+    }
+
+    @media screen and (max-width: ${DEVICE_SIZE.mobileM}) {
+      font-size: 40%;
+    }
+  }
+
+  body {
+    font-family: 'Karla',-apple-system, BlinkMacSystemFont, sans-serif;
+		font-size: 1.6rem;
+  }
+
+  a,
+  a:link,
+  a:hover,
+  a:focus {
+    color: currentColor;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  p {
+    line-height: 161%;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Montserrat Alternates',-apple-system, BlinkMacSystemFont, sans-serif;
+  }
+
+  h1 {
+    font-size: 6.4rem;
+  }
+
+  h2 {
+    font-size: 4rem;
+  }
+
+  h3 {
+    font-size: 3.6rem;
+  }
+
+  h4 {
+    font-size: 2.8rem;
+  }
+
+  h5 {
+    font-size: 2rem;
+  }
+
+  h6 {
+    font-size: 1.2rem;
+  }
+
+  p {
+    font-size: 1.8rem;
+  }
+`;
+
+export default GlobalStyle;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import '@components/common/Fonts/fonts.css';
+import GlobalStyle from '@components/common/GlobalStyles';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Aquifera</title>
+        <link rel="icon" href="/svg/aquifera-logo.svg" />
+      </Head>
+      <GlobalStyle />
+      <>
+        <Component {...pageProps} />
+      </>
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
