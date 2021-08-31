3aeedf15578c7a1c9f66cdb5b755dc448bfa5b96
Vigor Akbar <vigorakbar@gmail.com>
2021-08-31 22:14:35 +0700

    using const in header height

--- components/WhyWater/modules/components/Sidebar/sidebar.styled.ts
+++ components/WhyWater/modules/components/Sidebar/sidebar.styled.ts
@@ -1,9 +1,10 @@
+import { COMPONENT_SIZING_ON_REM } from '@constants/component-sizing.constant';
 import styled from 'styled-components';
 
 export const StyledSidebar = styled.div`
   background-color: white;
   position: sticky;
-  top: calc(7rem + 12px);
+  top: calc(${COMPONENT_SIZING_ON_REM.header}rem + 12px);
 
   hr {
     margin: 1.8rem 0;
