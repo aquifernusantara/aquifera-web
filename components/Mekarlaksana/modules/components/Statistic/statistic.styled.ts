adc7f8436802d3aa6083b9b4b5d60e66b7b5cf38
Vigor Akbar <vigorakbar@gmail.com>
2021-09-13 11:36:33 +0700

    update statistics responsive styling

--- components/Mekarlaksana/modules/components/Statistic/statistic.styled.ts
+++ components/Mekarlaksana/modules/components/Statistic/statistic.styled.ts
@@ -1,7 +1,13 @@
+import { DEVICE_SIZE } from '@constants/device-size.constant';
 import styled from 'styled-components';
 
 export const StyledStatistic = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: ${DEVICE_SIZE.tablet}) {
+    flex-direction: column;
+    margin: 6.4rem 0;
+  }
 `;
--- components/Mekarlaksana/modules/components/StatisticItem/statistic-item.styled.ts
+++ components/Mekarlaksana/modules/components/StatisticItem/statistic-item.styled.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import { COLOR } from '@constants/color.constant';
+import { DEVICE_SIZE } from '@constants/device-size.constant';
 
 export const StyledStatisticItem = styled.div`
   display: flex;
@@ -21,5 +22,20 @@ export const StyledStatisticItem = styled.div`
 
   :last-of-type {
     border-left: 0.1rem solid ${COLOR.border};
+  }
+
+  @media (max-width: ${DEVICE_SIZE.tablet}) {
+    border-bottom: 0.1rem solid ${COLOR.border};
+    padding: 1rem;
+    margin: 0;
+
+    :first-of-type {
+      border: 0;
+      border-bottom: 0.1rem solid ${COLOR.border};
+    }
+
+    :last-of-type {
+      border: 0;
+    }
   }
 `;
